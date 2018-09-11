daa6f5ef95454c7754b951a19bb3853f4796773f
mankek <krystal.manke@gmail.com>
2018-09-11 11:25:46 -0500

    changes

--- tracker/admin.py
+++ tracker/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Task, Time
 
 # Register your models here.
-admin.site.register(Task)
-admin.site.register(Time)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_text', 'performed_by')
+
+
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ('date_performed', 'time_hours', 'time_minutes', 'task_performed')
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Time, TimeAdmin)
